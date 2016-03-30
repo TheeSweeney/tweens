929df5e0758ee9844da901fe33c15e64a9f6b1fd
Brian Sweeney <bsweeney930@gmail.com>
2016-03-30 13:02:22 -0400

    wanted to have a commit with flexbox design

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// $(document).ready(function(){
-// 			var logo = document.getElementById("kerebel");
-// 	    console.log(logo);
-// 	    TweenLite.to(logo, 1, {"margin-right":"0px"});
-//   	})
+$(document).ready(function(){
+			var logo = document.getElementById("kerebel");
+	    console.log(logo);
+	    TweenLite.to(logo, 1, {"margin-right":"0px"});
+  	})
