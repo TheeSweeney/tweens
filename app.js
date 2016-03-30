acb1b662d8bec0c6721ed4c9f0d58525bb0e24ea
Brian Sweeney <bsweeney930@gmail.com>
2016-03-30 19:40:09 -0400

    filler text replaced with bacon ipsum for dribbler click. Image fading out and fading in. Not sure why TweenTo's delay property isnt working.

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
+
 $(document).ready(function(){
-			var picture = document.getElementById("kerebel");
-			var boxHeight = window.getComputedStyle(document.getElementById("txtImgLogoBox")).getPropertyValue('height')
-	    console.log(boxHeight);
-	    TweenLite.to(picture, 1, {"height": boxHeight});
-  	})
+//initial pageload
+	var picture = document.getElementById("kerebel");
+	//var boxHeight = window.getComputedStyle(document.getElementById("txtImgLogoBox")).getPropertyValue('height')
+  //console.log(boxHeight);
+  TweenLite.from(picture, 2, {"opacity": 0});
+
+
+
+
+//dribble
+	$('#dribble').click(function(){
+
+		var mainBox = document.getElementById("mainBox");
+		var kerebel = document.getElementById('kerebel');
+		
+		$("#bodyText").fadeOut(function() {
+	  	$(this).text("Pork prosciutto ball tip kielbasa, capicola cow flank beef turkey tail. Short loin turkey capicola t-bone, chicken alcatra picanha cupim kielbasa spare ribs shankle cow swine. Tri-tip shoulder kevin leberkas. Ham frankfurter swine salami t-bone pork ground round short ribs hamburger boudin chicken shank.").fadeIn();
+		});
+
+
+		var manToMetric = $("#kerebel").attr("src", "./styles/images/metrics.gif");
+
+		TweenLite.to(kerebel, 2, {
+			"opacity": 0,
+		})
+
+		TweenLite.to(kerebel, 2, {
+			"opacity": 1,
+			onComplete: manToMetric,
+		}).delay(2)
+	});
+
+
+
+
+
+
+
+
+})
+
