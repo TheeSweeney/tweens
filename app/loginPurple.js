449fa93ae1b6615010507b35fa38d3bfbeea80e5
Brian Sweeney <bsweeney930@gmail.com>
2016-04-01 15:41:38 -0400

    launching

---
+++ app/loginPurple.js
@@ -0,0 +1,24 @@
+
+	$('#loginPurpleDot').click(function(){
+		var signIn = document.getElementById('signIn');
+		console.log('here')
+		$('#signIn').click(function(){
+			console.log('sup')
+			// TweenLite.to(signIn, 1, {
+			// 	opacity: 0
+			// })
+		})
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
