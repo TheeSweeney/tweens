a059797afb9cc630f07fe7744fbdbb20fdb04092
Brian Sweeney <bsweeney930@gmail.com>
2016-04-07 16:58:37 -0400

    animation transition to first page mostly complete

--- app/loginPurple.js
+++ app/loginPurple.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	var textBox = document.getElementById('signInBox');
+	var signInBox = document.getElementById('signInBox');
 	var mountains = document.getElementById('mountains');
 	var topBars = document.getElementById('topBars');
 	var topGraphics = document.getElementById('topGraphics');
@@ -12,12 +12,12 @@ $(document).ready(function(){
 		var chain = new TimelineLite();
 
 		chain
-			.to(textBox, .5, {'border-radius': '0px'})
+			.to(signInBox, .5, {'border-radius': '0px'})
 			.to(topBars, .5, {
 				opacity: '0',
 				height: '0px',
 			})
-			.to(textBox, .5, {
+			.to(signInBox, .5, {
 				height: '247px',
 				width: '267px', 
 				'margin-top': '0px',
@@ -29,7 +29,7 @@ $(document).ready(function(){
 			.to(mountains, .5, {
 				'margin-top': '77px',
 				onComplete: function(){
-					$('#signInBox').html('<input type="text" class="input" id="Username" value="Username"/><input type="text" class="input" id="E-mail" value="E-mail"/><input type="text" class="input" id="Password" value="Password"/>')
+					$('#signInBox').html('<div id="textBox"><input type="text" class="input" id="Username" value="Username"/><input type="text" class="input" id="E-mail" value="E-mail"/><input type="text" class="input" id="Password" value="Password"/></div><div id="submit2Box"><div id="submit2"></div></div>')
 				
 					$('.input').focus(function(){
 						if(this.value == this.id){
@@ -45,18 +45,25 @@ $(document).ready(function(){
 
 					})
 
+					var submit2 = document.getElementById('submit2')
+
+
+					TweenLite.to(submit2, .5, {
+						width: '70px',
+						height: '35px',
+						'margin-top': '50px',
+						onComplete: function(){
+							$('#submit2').html('DONE')
+						}
+					})
+
 				}
 			}, '-=.25')
+			
 	})
 
 
 });
-
-
-
-// 77 mountain up 44 mt to textBox which is 244 high and 54 above bottom bars
-
-//247
 
 
 
